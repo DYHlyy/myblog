bda5be4d88487017833c8be0128ed838011a9b3d
lyy <yueyong1030@outlook.com>
2017-07-29 11:09:33 +0800

    最终完成了一个简易的博客

--- models/posts.js
+++ models/posts.js
@@ -4,6 +4,28 @@
 //存放与文章操作相关的代码：
 var marked = require('marked');
 var Post = require('../lib/mongo').Post;
+var CommentModel = require('./comments');
+
+//给post添加留言数commentsCount
+Post.plugin('addCommentsCount', {
+    afterFind: function (posts) {
+        return Promise.all(posts.map(function (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (commentsCount) {
+                post.commentsCount = commentsCount;
+                return post;
+            });
+        }));
+    },
+    afterFindOne: function (post) {
+        if (post) {
+            return CommentModel.getCommentsCount(post._id).then(function (count) {
+                post.commentsCount = count;
+                return post;
+            });
+        }
+        return post;
+    }
+});
 
 // 将 post 的 content 从 markdown 转换成 html
 Post.plugin('contentToHtml', {
@@ -30,9 +52,10 @@ module.exports = {
     // 通过文章 id 获取一篇文章
     getPostById: function getPostById(postId) {
         return Post
-            .findOne({ _id: postId })
-            .populate({ path: 'author', model: 'User' })
+            .findOne({_id: postId})
+            .populate({path: 'author', model: 'User'})
             .addCreatedAt()
+            .addCommentsCount()
             .contentToHtml()
             .exec();
     },
@@ -45,9 +68,10 @@ module.exports = {
         }
         return Post
             .find(query)
-            .populate({ path: 'author', model: 'User' })
-            .sort({ _id: -1 })
+            .populate({path: 'author', model: 'User'})
+            .sort({_id: -1})
             .addCreatedAt()
+            .addCommentsCount()
             .contentToHtml()
             .exec();
     },
@@ -55,7 +79,7 @@ module.exports = {
     // 通过文章 id 给 pv 加 1
     incPv: function incPv(postId) {
         return Post
-            .update({ _id: postId }, { $inc: { pv: 1 } })
+            .update({_id: postId}, {$inc: {pv: 1}})
             .exec();
     },
 
@@ -74,6 +98,13 @@ module.exports = {
 
     // 通过用户 id 和文章 id 删除一篇文章
     delPostById: function delPostById(postId, author) {
-        return Post.remove({author: author, _id: postId}).exec();
+        return Post.remove({author: author, _id: postId})
+            .exec()
+            .then(function (res) {
+                //文章删除后，在删除文章下的所有留言
+                if (res.result.ok && res.result.n > 0) {
+                    return CommentModel.delCommentsByPostId(postId);
+                }
+            });
     }
 };
